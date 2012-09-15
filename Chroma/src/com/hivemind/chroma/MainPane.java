2cf264c30b9e8c0629490d3e02e1d5fd19db1bf6
Cary Yang <caryy@cmu.edu>
2012-09-15 01:56:23 -0400

    Created a MainPane with a SurfaceView.

---
+++ Chroma/src/com/hivemind/chroma/MainPane.java
@@ -0,0 +1,14 @@
+package com.hivemind.chroma;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainPane extends Activity {
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_pane);
+    }
+}
